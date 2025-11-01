9aae601be07222ba48c3d2ea91cb6381f43c2096
Your Name <keerthanajaikumar98@gmail.com>
2025-11-01 18:25:56 -0400

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Creating Directory
+mkdir -p src
+mkdir -p research
+
+#Creating Files
+touch src/__init__.py #constructor file
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directories and Files Created!"
