e8ad8327a61ecdef063b2a3cbaf92d45c302246c
Your Name <keerthanajaikumar98@gmail.com>
2025-11-01 18:38:17 -0400

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Keerthana Jaikumar",
+    author_email="keerthanajaikumar98@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
